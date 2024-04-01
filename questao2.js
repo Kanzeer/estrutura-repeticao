794d3a948194e70b3b0d898467556b94118e3c2f
Kanzeer <miguelroaamaral@gmail.com>
2024-04-01 15:35:22 -0400

    questão 2 finalizada

---
+++ questao2.js
@@ -0,0 +1,7 @@
+//2. Questão: Escreva um código que imprima os números pares de 2 a 10
+
+for(let pares = 2; pares <= 10; pares++) {
+    if(pares % 2 == 0) {
+    console.log("número par: " + pares);
+    }
+}
